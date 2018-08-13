07873780c6fb6280329e95cf5427de2fb907f170
andy <wanghaoxin5277@gmail.com>
2018-08-13 23:57:03 +0800

    initiliaztion

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+      Hello world
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      return {}
+    }
+  }
+</script>
+<style scoped lang="scss">
+
+</style>
