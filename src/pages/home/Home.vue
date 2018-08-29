007322b85c8a9291302c4a1d2738c8287ebad2ed
andy <wanghaoxin5277@gmail.com>
2018-08-29 20:55:16 +0800

    ajax

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -6,29 +6,42 @@
       <home-icons></home-icons>
       <home-recommend></home-recommend>
       <home-weekend></home-weekend>
-     </div>
+    </div>
   </div>
 </template>
 <script>
-  import HomeHeader from './components/Header'
-  import HomeSwiper from './components/Swiper'
-  import HomeIcons from './components/Icons'
-  import HomeRecommend from './components/Recommend'
-  import HomeWeekend from './components/Weekend'
-  export default {
-    name: 'Home',
-    data() {
-      return {}
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
+import axios from 'axios'
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  components: {
+    HomeIcons,
+    HomeSwiper,
+    HomeHeader,
+    HomeRecommend,
+    HomeWeekend,
+    axios
+  },
+  methods: {
+    getHomeInfo() {
+      axios.get('/api/index.json')
+        .then(this.getHomeInfoSuccess)
     },
-    components: {
-      HomeIcons,
-      HomeSwiper,
-      HomeHeader,
-      HomeRecommend,
-      HomeWeekend
+    getHomeInfoSuccess(res) {
+        console.log(res.data.name);
     }
+  },
+  mounted() {
+    this.getHomeInfo();
   }
+}
 </script>
 <style scoped lang="scss">
-
 </style>
